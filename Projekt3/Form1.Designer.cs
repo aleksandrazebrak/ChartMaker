1838626c7aa0c7029847edd7362b4e10c7d3bb76
olaze <olaze@DESKTOP-5AC2JQL>
2021-10-28 14:20:38 +0200

    Dodaj pliki projektów.

---
+++ Projekt3/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Projekt3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 73);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.MarkerSize = 2;
+            series1.Name = "Series1";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Label = "y =  #VAL{N2}\\nx = #VALX{N2}";
+            series2.Legend = "Legend1";
+            series2.MarkerSize = 6;
+            series2.Name = "Series2";
+            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
+            series3.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series3.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(745, 443);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Otwórz plik";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(504, 230);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(8, 8);
+            this.chart2.TabIndex = 2;
+            this.chart2.Text = "chart2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(96, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 55);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 528);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        public System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Projekt3/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+
+namespace Projekt3
+{
+    
+
+    public partial class Form1 : Form
+    {
+       string[] elementy = {};
+        int iloscElementow = 0;
+        bool done = true;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+            openFileDialog1.Filter = "xy files(*.xy)|*.xy";
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                done = false;
+                chart1.Series["Series1"].Points.Clear();
+                chart1.Series["Series2"].Points.Clear();
+                chart1.Series["Series3"].Points.Clear();
+                System.IO.StreamReader myStreamReader = new
+                System.IO.StreamReader(openFileDialog1.FileName);
+
+
+                
+                elementy = myStreamReader.ReadToEnd().Split(new char[] {' ', '\n'});
+                
+                iloscElementow = elementy.Length;
+                
+                PointF myPoint = new PointF();
+
+
+
+                
+                for (int i = 0; i < iloscElementow - 3; i = i+2)
+                {
+
+                    
+                    myPoint.X = float.Parse(elementy[i],new CultureInfo("en-US"));
+                    myPoint.Y = float.Parse(elementy[i + 1], new CultureInfo("en-US"));
+                    
+                    chart1.Series["Series1"].Points.AddXY(myPoint.X, myPoint.Y);
+                    
+
+                     
+                  }
+                
+                
+                  
+                
+                myStreamReader.Close();
+
+                    
+
+                    
+                
+                
+            }
+        }
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+
+            if (done == false)
+            {
+                float wartoscNajwyzsza = float.Parse(elementy[0], new CultureInfo("en-US"));
+                int wartoscNajwyzszaNr = 0;
+                for (int i = 1; i < iloscElementow - 3; i = i + 2)
+                {
+
+
+                    if (float.Parse(elementy[i], new CultureInfo("en-US")) > wartoscNajwyzsza)
+                    {
+                        wartoscNajwyzsza = float.Parse(elementy[i], new CultureInfo("en-US"));
+                        wartoscNajwyzszaNr = i;
+                    }
+                }
+
+                int wartoscNajwyzszaNrX = wartoscNajwyzszaNr - 1;
+                float Xnajwyzsza = float.Parse(elementy[wartoscNajwyzszaNrX], new CultureInfo("en-US"));
+
+                chart1.Series["Series2"].Points.AddXY(Xnajwyzsza, wartoscNajwyzsza);
+
+                float wartoscNajnizsza = float.Parse(elementy[1], new CultureInfo("en-US"));
+                int wartoscNajnizszaNr = 0;
+
+                for (int i = 1; i < iloscElementow - 3; i = i + 2)
+                {
+
+
+                    if (float.Parse(elementy[i], new CultureInfo("en-US")) < wartoscNajnizsza)
+                    {
+                        wartoscNajnizsza = float.Parse(elementy[i], new CultureInfo("en-US"));
+                        wartoscNajnizszaNr = i;
+                    }
+                }
+
+                int wartoscNajnizszaNrX = wartoscNajnizszaNr - 1;
+                float Xnajnizsza = float.Parse(elementy[wartoscNajnizszaNrX], new CultureInfo("en-US"));
+
+                chart1.Series["Series2"].Points.AddXY(Xnajnizsza, wartoscNajnizsza);
+
+                float fPolowa = (wartoscNajwyzsza - wartoscNajnizsza) / 2;
+
+                
+
+                chart1.Series["Series3"].Points.AddXY(chart1.ChartAreas[0].AxisX.Minimum, fPolowa);
+                chart1.Series["Series3"].Points.AddXY(chart1.ChartAreas[0].AxisX.Maximum, fPolowa);
+
+
+                chart1.Series["Series3"].Points[0].Label = "1/2 max = #VAL{N2}";
+                done = true;
+
+                
+                
+                //------chujowe poczatki szukania punktow-------------//
+                
+                PointF najblizszyPrawoGora = new PointF();
+                
+
+
+                float MojaRoznicaWartosci = Math.Abs(float.Parse(elementy[1], new CultureInfo("en-US")) - fPolowa);
+                float MojaRoznicaWartosci2 = Math.Abs(float.Parse(elementy[1], new CultureInfo("en-US")) - fPolowa);
+
+                for (int i = wartoscNajwyzszaNr; i < iloscElementow - 3; i = i + 2)
+                {
+                   float roznica = float.Parse(elementy[i], new CultureInfo("en-US")) - fPolowa;
+
+                    if (roznica < MojaRoznicaWartosci && roznica > 0)
+                    {
+                       MojaRoznicaWartosci = roznica;
+                       najblizszyPrawoGora.X = float.Parse(elementy[i - 1], new CultureInfo("en-US"));
+                       najblizszyPrawoGora.Y = float.Parse(elementy[i], new CultureInfo("en-US"));
+                   }
+                
+                }
+
+               
+                    
+                    chart1.Series["Series2"].Points.AddXY(najblizszyPrawoGora.X, najblizszyPrawoGora.Y);
+
+
+
+
+
+
+
+                   
+                }
+               
+
+                
+                
+
+
+           
+
+
+               }
+
+               
+                
+            
+            
+            
+            }
+                
+            
+           
+            
+        }
+
+                      
+                
+
+               
+              
+
+    
+
+
+      
+    
+
+
+
+
